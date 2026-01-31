346aa7312bdbe10a0d3710c2e14be4f4ba076e07
SIMPle <arshj2006@gmail.com>
2026-01-31 07:25:33 +0530

    jan 27

---
+++ src/with_static.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ class MyClass {
+    static int n;
+    //    int n;
+    public static void main(String args[]) {
+        Scanner s = new Scanner (System.in);
+//    Main m = new Main();
+//    int n;
+//    mn = s.nextInt();
+        n = s.nextInt();
+        System.out.println(n);
+//    System.out.println(mn);
+    }
---
+++ src/without_static_A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class Main {
+    //    static int n;
+    int n;
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        Main m = new Main();
+//    int n;
+        m.n = s.nextInt();
+// n = s.nextInt();
+//    System.out.println(n);
+        System.out.println(m.n);
+    }
+}
